7681c68fa7bba1ecd6e92ddd2eb807fd56184e59
venky04-d <venky04102005@gmail.com>
2026-09-23 22:59:50 +0530

    Fix production server startup

--- app.py
+++ app.py
@@ -401,9 +401,10 @@ create_database()
 
 # ---------------- START FLASK ----------------
 
-import os
+if __name__ == "__main__":
+    import os
 
-app.run(
-    host="0.0.0.0",
-    port=int(os.environ.get("PORT", 5000))
-)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
